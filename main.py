8354470d2cb60fd4b614e10565c9ecece3ea8780
OlegVard <61628812+OlegVard@users.noreply.github.com>
2022-06-19 01:54:23 +0700

    add filter, witch make acid img, reduce img and make it bigger. Delete old stuff, cos it's too slow

--- main.py
+++ main.py
@@ -9,65 +9,57 @@ def big_pixels(img_path, mask):
     img_out = np.zeros_like(img)
     for i in range(0, img.shape[0] - mask, mask):
         for j in range(0, img.shape[1] - mask, mask):
-            r = np.mean(img[i:i+mask, j:j+mask, 0])
-            g = np.mean(img[i:i+mask, j:j+mask, 1])
-            b = np.mean(img[i:i+mask, j:j+mask, 2])
-            img_out[i:i+mask, j:j+mask] = [r, g, b]
+            r = np.mean(img[i:i + mask, j:j + mask, 0])
+            g = np.mean(img[i:i + mask, j:j + mask, 1])
+            b = np.mean(img[i:i + mask, j:j + mask, 2])
+            img_out[i:i + mask, j:j + mask] = [r, g, b]
     cv2.imwrite('pix_av.jpg', img_out[0:new_h, 0:new_w, :])
 
 
+def reduce_pic(img_path, mask):
+    img = np.array(cv2.imread(img_path))
+    new_h = (img.shape[0] // mask - 1) * mask
+    new_w = (img.shape[1] // mask - 1) * mask
+    img = img[0:new_h, 0:new_w, :]
+    img_out = np.zeros_like(img)
+    for i in range(img.shape[0] // mask):
+        for j in range(img.shape[1] // mask):
+            img_out[i, j] = img[i * mask, j * mask]
+    cv2.imwrite('small_img.jpg', img_out[0:int(new_h / mask), 0:int(new_w / mask), :])
+
+
+def upscale_img(img_path, mask):
+    img = np.array(cv2.imread(img_path))
+    new_h = img.shape[0] * mask
+    new_w = img.shape[1] * mask
+    img = img[0:new_h, 0:new_w, :]
+    img_out = np.zeros([new_h, new_w, 3])
+    for i in range(img_out.shape[0]):
+        for j in range(img_out.shape[1]):
+            img_out[i, j] = img[i // mask, j // mask]
+    cv2.imwrite('big_img.jpg', img_out)
+
+
+def acid(img_path, koef):
+    img = np.array(cv2.imread(img_path))
+    img_out = np.zeros_like(img)
+    for i in range(1, img.shape[0] - 1):
+        for j in range(1, img.shape[1] - 1):
+            img_out[i, j] = (koef * img[i, j] + img[i - 1, j] + img[i + 1, j] + img[i, j - 1] + img[i, j + 1])
+    cv2.imwrite('acid_img2.jpg', img_out)
+
+
 def bound(img_path):
-    mask = np.array([[-1, 1, -1],
-                    [1, -1, 1],
-                    [-1, 1, -1]])
     img = np.array(cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY))
-    if cv2.countNonZero(np.where(img > 128, 1, 0)) > cv2.countNonZero(np.where(img > 128, 0, 1)):
-        img = np.where(img > 128, 0, 1)
-    else:
-        img = np.where(img < 128, 0, 1)
-
-    while cv2.countNonZero(img) != 0:
-        count_of_zero = cv2.countNonZero(img)
-        obr_img = ~hit_miss(img, mask)
-        img = img & obr_img
-        if count_of_zero == cv2.countNonZero(img):
-            cv2.imwrite('Avatar.jpg', img*255)
-            break
-
-
-def hit_miss(img_, mask):
-    subset = erosion(img_, mask)
-    subset2 = erosion(1 - img_, 1 - mask)
-    return subset & subset2
-
-
-def erosion(img_, kernel):
-    ker_t = np.array([[0, 0, 0],
-                      [0, 1, 0],
-                      [0, 0, 0]])
-    ker_f = np.array([[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 0, 0]])
-    ret_img = np.zeros_like(img_)
-    for i in range(img_.shape[0] - 2):
-        for j in range(img_.shape[1] - 2):
-            if array_eq(img_[i:i + 3, j:j + 3], kernel):
-                ret_img[i:i + 3, j:j + 3] = ret_img[i:i + 3, j:j + 3] | ker_t
-            else:
-                ret_img[i:i + 3, j:j + 3] = ret_img[i:i + 3, j:j + 3] | ker_f
-    return ret_img
-
-
-def array_eq(img_shape, kernel):
-    for i in range(img_shape.shape[0]):
-        for j in range(img_shape.shape[1]):
-            if img_shape[i, j] == kernel[i, j]:
-                continue
-            elif kernel[i, j] == -1 or kernel[i, j] == 2:
-                continue
-            else:
-                return False
-    return True
-
-
-big_pixels('og.jpg', 15)
+    img_out = np.zeros_like(img)
+    prev_matrix_1 = np.array([[-1, -1, -1],
+                              [0, 0, 0],
+                              [1, 1, 1]])
+    prev_matrix_2 = np.array([[-1, 0, 1],
+                              [-1, 0, 1],
+                              [-1, 0, 1]])
+    for i in range(1, img_out.shape[0] - 1):
+        for j in range(1, img_out.shape[1] - 1):
+            img_out[i, j] = (np.abs(np.sum(img[i - 1:i + 2, j - 1:j + 2] * prev_matrix_1)) +
+                             np.abs(np.sum(img[i - 1:i + 2, j - 1:j + 2] * prev_matrix_2))) / 2
+    cv2.imwrite('bound_img.jpg', img_out[0:img_out.shape[0]-2, 0:img_out.shape[1]-2])
